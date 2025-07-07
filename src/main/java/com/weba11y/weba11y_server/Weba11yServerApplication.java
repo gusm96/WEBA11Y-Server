ea0255207b1d28d803128dc26b5ea1758743e58c
Seongmo Gu <gusm96@naver.com>
2025-07-07 13:45:58 +0900

    Initial commit of Spring WebFlux WEBA11Y project

---
+++ src/main/java/com/weba11y/weba11y_server/Weba11yServerApplication.java
@@ -0,0 +1,13 @@
+package com.weba11y.weba11y_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Weba11yServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Weba11yServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weba11y/weba11y_server/Weba11yServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weba11y.weba11y_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Weba11yServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
